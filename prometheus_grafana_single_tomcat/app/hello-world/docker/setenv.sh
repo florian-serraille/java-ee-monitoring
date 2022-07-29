7f37d06f623d872d01136446b12201cbbad8426e
Florian <florian.serraille@dock.teck>
2022-07-29 07:16:58 -0300

    Lightstep configuration

---
+++ prometheus_grafana_single_tomcat/app/hello-world/docker/setenv.sh
@@ -0,0 +1,3 @@
+export LS_ACCESS_TOKEN=rYPEN/OhSq+uRK9XSmHCzxLlBReVMmEsIVQxI9W8zjjOj6l6dhEdhoFHMP7yui94+u++Ep3/HRNPqCmxowzr23HdkMl0/Bk4IyTmPa4m
+export LS_SERVICE_NAME=hello-world
+CATALINA_OPTS="$CATALINA_OPTS -javaagent:/usr/local/tomcat/lib/lightstep-opentelemetry-javaagent.jar"
